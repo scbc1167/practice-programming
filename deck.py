2c04073e90569458b09317d295293ac3559b21aa
scbc1167 <scbc1167@gmail.com>
2019-06-11 07:53:56 +0900

    my second commit puls

---
+++ deck.py
@@ -0,0 +1,24 @@
+# Thanks so much for reading my book. Feel free to contact me at cory[at]theselftaughtprogrammer.io.
+
+
+from random import shuffle
+
+
+class Deck:
+    def __init__(self):
+        self.cards = []
+        for i in range(2, 15):
+            for j in range(4):
+                self.cards\
+                    .append(Card(i,
+                                 j))
+        shuffle(self.cards)
+
+    def rm_card(self):
+        if len(self.cards) == 0:
+            return
+        return self.cards.pop()
+
+deck = Deck()
+for card in deck.cards:
+    print(card)
